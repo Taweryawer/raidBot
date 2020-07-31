cac6df2ebce8ae41dbcc2f58319846c1a7c99202
Taweryawer <vlad707020@gmail.com>
2020-07-31 05:20:40 -0700

    ReplyKeyboardBuilder created

---
+++ src/main/java/taweryawer/util/ReplyKeyboardBuilder.java
@@ -0,0 +1,32 @@
+package taweryawer.util;
+
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ReplyKeyboardBuilder {
+
+    private List<KeyboardRow> keyboard = new ArrayList<>();
+    private KeyboardRow currentRow = new KeyboardRow();
+
+    public void addButton(String title) {
+        if(currentRow.size() >= 2) {
+            keyboard.add(currentRow);
+            currentRow = new KeyboardRow();
+        }
+        currentRow.add(new KeyboardButton(title));
+    }
+
+    public ReplyKeyboardMarkup build() {
+        keyboard.add(currentRow);
+        ReplyKeyboardMarkup markup = new ReplyKeyboardMarkup(keyboard);
+        currentRow = new KeyboardRow();
+        keyboard = new ArrayList<>();
+        return markup;
+    }
+}
